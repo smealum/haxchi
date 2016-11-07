73c327adf27abda33a16c0c7e4848f9069bda624
FIX94 <fix94.1@gmail.com>
2016-11-08 00:37:34 +0100

    ported over to the pal version of brain age

--- haxchi.s
+++ haxchi.s
@@ -1,7 +1,7 @@
 .create "haxchi.srl", 0
 .nds
 
-hax_target_address equ 0x107968AC
+hax_target_address equ 0x1076FAA4
 code_target_address equ (0xF4000000 + 0xFD2000)
 
 .org 0x000
@@ -15,11 +15,11 @@ code_target_address equ (0xF4000000 + 0xFD2000)
 .org 0x020
 	.word arm9_data ; ARM9 rom_offset
 	.word 0x20000000 ; ARM9 entry_address
-	.word 0xEBBC0E00 + code_target_address ; ARM9 ram_address
+	.word 0xEBDDFC00 + code_target_address ; ARM9 ram_address
 	.word arm9_data_end - arm9_data ; ARM9 size
 	.word arm7_data ; ARM7 rom_offset
 	.word 0x2000000 ; ARM7 entry_address
-	.word 0xEBBC0E00 + hax_target_address ; ARM7 ram_address
+	.word 0xEBDDFC00 + hax_target_address ; ARM7 ram_address
 	.word arm7_data_end - arm7_data ; ARM7 size
 
 .org 0x080
